d9df9d59eed26917105e6c088ab4f34198b9f06d
Alessandro Tulioli <desmecad@hotmail.com>
2020-05-28 21:29:09 -0300

    Commit 2 - Criada a classe que implementa os métodos GET - GET{id} e POST

--- src/main/java/com/produtos/apirest/repository/ProdutoRepository.java
+++ src/main/java/com/produtos/apirest/repository/ProdutoRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.produtos.apirest.models.Produtos;
 
 public interface ProdutoRepository extends JpaRepository<Produtos, Long>{
+	
+	Produtos findById(long id);
 
 }

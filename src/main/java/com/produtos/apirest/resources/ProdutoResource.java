03474cb1bae10ac5edddab7661524ca1da2d9542
Alessandro Tulioli <desmecad@hotmail.com>
2020-05-28 21:43:33 -0300

    Commit 2 - Implementados os métodos PUT e DELETE

--- src/main/java/com/produtos/apirest/resources/ProdutoResource.java
+++ src/main/java/com/produtos/apirest/resources/ProdutoResource.java
@@ -3,9 +3,11 @@ package com.produtos.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +36,15 @@ public class ProdutoResource {
 	public Produtos salvaProduto(@RequestBody Produtos produto){
 		return produtoRepository.save(produto);
 	}
+	
+	@DeleteMapping("/produto")
+	public void deletaProduto(@RequestBody Produtos produto){
+		produtoRepository.delete(produto);
+	}
 
+	@PutMapping("/produto")
+	public Produtos atualizaProduto(@RequestBody Produtos produto){
+		return produtoRepository.save(produto);
+	}
 
 }
